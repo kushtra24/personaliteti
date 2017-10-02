46f8fcc73730cc982445fdfc232d3f271f48d585
Kushtrim <k.ramadani@beo-kosovo.com>
2017-10-02 15:27:03 +0200

    nothing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="login-container">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -65,5 +66,6 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
