8d6166a467131ee142be9bb0d3a09600bbde150a
kushtrim <kushtra24@gmail.com>
2018-03-19 09:18:56 +0100

    result page still working

--- database/seeds/questionsSeeder.php
+++ database/seeds/questionsSeeder.php
@@ -14,26 +14,266 @@ class questionsSeeder extends Seeder
     {
         DB::table('Questions')->insert([
             'question' => "question one",
-	        'purpose' => "extroIntro",
+	        'purpose' => "IntroExtro",
 	        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('Questions')->insert([
             'question' => "question two",
-	        'purpose' => "extroIntro",
+	        'purpose' => "intuitionSensing",
 	        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('Questions')->insert([
             'question' => "question three",
-	        'purpose' => "extroIntro",
+	        'purpose' => "feelingThinking",
 	        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('Questions')->insert([
             'question' => "question four",
-	        'purpose' => "extroIntro",
+	        'purpose' => "jundgingPerciving",
 	        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question five",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question six",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question seven",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 8",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 9",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 10",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 11",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 12",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 13",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 14",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 15",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 16",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 17",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 18",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 19",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 20",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 21",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 22",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+         DB::table('Questions')->insert([
+            'question' => "question 23",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 24",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question one",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question two",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question three",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question four",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question five",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question six",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question seven",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 8",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 9",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 10",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 11",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 12",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 13",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 14",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 15",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 16",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 17",
+            'purpose' => "IntroExtro",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 18",
+            'purpose' => "intuitionSensing",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 19",
+            'purpose' => "feelingThinking",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('Questions')->insert([
+            'question' => "question 20",
+            'purpose' => "jundgingPerciving",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
     }
 }
--- resources/views/testi/result.blade.php
+++ resources/views/testi/result.blade.php
@@ -1,56 +1,138 @@
 @extends('layouts.app')
 
+@section('style')
+<style>
+  #container { text-align: center; margin: 20px; }
+h2 { color: #CCC; }
+a { text-decoration: none; color: #EC5C93; }
+
+.bar-main-container {
+  margin: 10px auto;
+  /*width: 300px;*/
+  height: 50px;
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+  font-family: sans-serif;
+  font-weight: normal;
+  /*font-size: 0.8em;*/
+  color: #FFF;
+}
+
+.wrap { padding: 8px; }
+
+.bar-percentage {
+  float: left;
+  background: rgba(0,0,0,0.13);
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+  padding: 9px 0px;
+  width: 18%;
+  height: 16px;
+}
+
+.bar-container {
+  float: right;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px;
+  height: 10px;
+  background: rgba(0,0,0,0.13);
+  width: 78%;
+  margin: 12px 0px;
+  overflow: hidden;
+}
+
+.bar {
+  float: left;
+  background: #FFF;
+  height: 100%;
+  -webkit-border-radius: 10px 0px 0px 10px;
+  -moz-border-radius: 10px 0px 0px 10px;
+  border-radius: 10px 0px 0px 10px;
+  -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+  filter: alpha(opacity=100);
+  -moz-opacity: 1;
+  -khtml-opacity: 1;
+  opacity: 1;
+}
+
+/* COLORS */
+.azure   { background: #38B1CC; }
+.emerald { background: #2CB299; }
+.violet  { background: #8E5D9F; }
+.yellow  { background: #EFC32F; }
+.red     { background: #E44C41; }
+</style>
+@endsection
+
 @section('content')
 
 <div class="container">
 
-<h1>Ti je një: @if ($finalType == null) {{ Cookie::get('finaltype') }} @else {{ $finalType }} @endif</h1>
+<div class="col-md-4">
+  <h1>Ti je një: @if ($finalType == null) {{ Cookie::get('finaltype') }} @else {{ $finalType }} @endif</h1>
 
-  @if($FirstfinalProcentRez)
-<h3> {{ $FirstfinalProcentRez }}% {{ $introExtro }} </h3>
-<h3>{{ $nsfinalProcentRez }}% {{ $intuSens }}</h3>
-<h3> {{ $ftfinalProcentRez }}% {{$feelingThinking}}</h3>
-<h3>{{ $jpfinalProcentRez }}% {{$judgingPerspecting}}</h3>
-
-
-<div class="traid"><h4>Mendimet</h4></div>
-<div class="traid-discription"><p>Kjo veti tregon se si veprojme me ambientin</p></div>
-<div class="progress">
-  <div class="progress-bar {{ $introExtro }}" role="progressbar" aria-valuenow="{{ $FirstfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $FirstfinalProcentRez}}%;">
-    {{ $FirstfinalProcentRez }}% {{ $introExtro }}
-  </div>@if ($FirstfinalProcentRez == "100")
-         {{" "}}
-    @else
-    <span>{{ 100 - $FirstfinalProcentRez }}% {{ $introExtro === "Extrovert" ? "Introvert" : "Extrovert" }}</span>
-    @endif
+    @if($FirstfinalProcentRez)
+  <h3> {{ $FirstfinalProcentRez }}% {{ $introExtro }} </h3>
+  <h3>{{ $nsfinalProcentRez }}% {{ $intuSens }}</h3>
+  <h3> {{ $ftfinalProcentRez }}% {{$feelingThinking}}</h3>
+  <h3>{{ $jpfinalProcentRez }}% {{$judgingPerspecting}}</h3>
 </div>
 
-<div class="traid"><h4>Energjia</h4></div>
-<div class="traid-discription"><p>Kjo veti tregon ku ne perciellim energjien mentale</p></div>
-<div class="progress">
-  <div class="progress-bar" role="progressbar" aria-valuenow="{{ $nsfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $nsfinalProcentRez}}%;">
-    {{ $nsfinalProcentRez }}% {{$intuSens}}
+<div class="col-md-8">
+  <div class="traid"><h4>Mendimet</h4></div>
+  <div class="traid-discription"><p>Kjo veti tregon se si veprojme me ambientin</p></div>
+
+  <div class="progress">
+    <div class="progress-bar {{ $introExtro }}" role="progressbar" aria-valuenow="{{ $FirstfinalProcentRez }}"  aria-valuemin="0" aria-valuemax="100" style="width: {{ $FirstfinalProcentRez}}%;">
+
+      {{ $FirstfinalProcentRez }}% {{ $introExtro }}
+    </div>@if ($FirstfinalProcentRez == "100")
+           {{" "}}
+      @els
+  e    <span>{{ 100 - $FirstfinalProcentRez }}% {{ $introExtro === "Extrovert" ? "Introvert" : "Extrovert" }}</span>
+      @endif
   </div>
-      <span>{{ 100 - $nsfinalProcentRez }}% {{ $intuSens === "Shqisor" ? "Intuitive" : "Shqisor" }}</span>
+
+    <div id="bar-1" class="bar-main-container azure">
+      <div class="wrap">
+        <div class="bar-percentage" data-percentage="{{ $FirstfinalProcentRez }}"></div>
+        <div class="bar-container">
+          <div class="bar"></div>
+        </div>
+      </div>
+    </div>
+
+
+  <div class="traid"><h4>Energjia</h4></div>
+  <div class="traid-discription"><p>Kjo veti tregon ku ne perciellim energjien mentale</p></div>
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" aria-valuenow="{{ $nsfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $nsfinalProcentRez}}%;">
+      {{ $nsfinalProcentRez }}% {{$intuSens}}
+    </div>
+        <span>{{ 100 - $nsfinalProcentRez }}% {{ $intuSens === "Shqisor" ? "Intuitive" : "Shqisor" }}</span>
+  </div>
+
+  <div class="traid"><h4>Natyrshmeria</h4></div>
+  <div class="traid-discription"><p>Kjo veti tregon se si marrim vendime dhe perballemi me emocione </p></div>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="{{ $ftfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $ftfinalProcentRez}}%;">
+        {{ $ftfinalProcentRez }}% {{$feelingThinking}}
+      </div>
+            <span>{{ 100 - $ftfinalProcentRez }}% {{ $feelingThinking === "Ndjenjë (Feeling)" ? "Mendim (Thinking)" : "Ndjenjë (Feeling)" }}</span>
+    </div>
+
+  <div class="traid"><h4>Teknika</h4></div>
+  <div class="traid-discription"><p>Kjo veti tregon se si Marrim vendime</p></div>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar" aria-valuenow="{{ $jpfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $jpfinalProcentRez}}%;">
+        {{ $jpfinalProcentRez }}% {{$judgingPerspecting}}
+      </div>
+          <span>{{ 100 - $jpfinalProcentRez }}% {{ $judgingPerspecting === "Perspektivë" ? "Gjykues" : "Perspektivë" }}</span>
+    </div>
 </div>
-
-<div class="traid"><h4>Natyrshmeria</h4></div>
-<div class="traid-discription"><p>Kjo veti tregon se si marrim vendime dhe perballemi me emocione </p></div>
-  <div class="progress">
-    <div class="progress-bar" role="progressbar" aria-valuenow="{{ $ftfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $ftfinalProcentRez}}%;">
-      {{ $ftfinalProcentRez }}% {{$feelingThinking}}
-    </div>
-          <span>{{ 100 - $ftfinalProcentRez }}% {{ $feelingThinking === "Ndjenjë (Feeling)" ? "Mendim (Thinking)" : "Ndjenjë (Feeling)" }}</span>
-  </div>
-
-<div class="traid"><h4>Teknika</h4></div>
-<div class="traid-discription"><p>Kjo veti tregon se si Marrim vendime</p></div>
-  <div class="progress">
-    <div class="progress-bar" role="progressbar" aria-valuenow="{{ $jpfinalProcentRez }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $jpfinalProcentRez}}%;">
-      {{ $jpfinalProcentRez }}% {{$judgingPerspecting}}
-    </div>
-        <span>{{ 100 - $jpfinalProcentRez }}% {{ $judgingPerspecting === "Perspektivë" ? "Gjykues" : "Perspektivë" }}</span>
-  </div>
     @else
     <div class="alert alert-info" role="alert">Vazhdo Regjistrimi resultatet e juaja jane ende aktive</div>
   @endif
@@ -185,5 +267,27 @@
     $("#registerForma").removeClass("hide");
     $("#show_register").addClass("hide");
   });
+
+
+  // procentage animation
+  $('.bar-percentage[data-percentage]').each(function () {
+  var progress = $(this);
+  var percentage = Math.ceil($(this).attr('data-percentage'));
+  $({countNum: 0}).animate({countNum: percentage}, {
+    duration: 2000,
+    easing:'linear',
+    step: function() {
+      // What todo on every count
+    var pct = '';
+    if(percentage == 0){
+      pct = Math.floor(this.countNum) + '%';
+    }else{
+      pct = Math.floor(this.countNum+1) + '%';
+    }
+    progress.text(pct) && progress.siblings().children().css('width',pct);
+    }
+  });
+});
+
   </script>
 @endsection
