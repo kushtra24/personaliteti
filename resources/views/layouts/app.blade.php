17c57526013afb41f0a4cf950c2a361fc0132fc1
Kushtrim <Kushtra24@gmail.com>
2018-04-02 16:17:27 +0200

    logo changed, a new migration added for the media library

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{--{{ config('app.name', 'Personaliteti') }}--}}
-                        <img src="{{asset('images/personaliteti-logo.png')}}" height="30px" alt="personaliteti.com">
+                        <img src="{{asset('images/logo.png')}}" height="50px" alt="personaliteti.com">
                     </a>
                 </div>{{-- nabar-header --}}
 
